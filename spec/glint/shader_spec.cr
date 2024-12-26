2643b07b52cc08d71459ad25383e529a6f3f325b
Michael Miller <icy.arctic.fox@gmail.com>
2024-12-26 16:59:08 -0700

    Implement and test Shader

--- spec/glint/shader_spec.cr
+++ spec/glint/shader_spec.cr
@@ -2,8 +2,30 @@ require "../spec_helper"
 
 alias Shader = Glint::Shader
 
+VALID_SHADER_SOURCE = <<-GLSL
+void main() {
+  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+}
+GLSL
+
+INVALID_SHADER_SOURCE = <<-GLSL
+void main() {
+  gl_Position = vec3(0.0, 0.0, 0.0);
+}
+GLSL
+
 Spectator.describe Shader do
   let gl_context = TestOpenGLScaffold.context
+
+  describe "#initialize" do
+    it "sets the attributes" do
+      shader = Shader.new(gl_context, 42)
+      expect(shader).to have_attributes(
+        context: gl_context,
+        name: 42,
+      )
+    end
+  end
 
   describe "#exists?" do
     it "returns true if the shader exists" do
@@ -14,6 +36,100 @@ Spectator.describe Shader do
     it "returns false if the shader does not exist" do
       shader = Shader.new(gl_context, 0)
       expect(shader.exists?).to be_false
+    end
+  end
+
+  describe "#delete" do
+    it "deletes the shader" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.delete
+      expect(shader.exists?).to be_false
+    end
+  end
+
+  describe "#deleted?" do
+    it "returns true if the shader is deleted", skip: "Requires Program to be implemented" do
+      # To get into a deleted state, the shader must be in use when deleted.
+      shader = Shader.new(gl_context, :vertex)
+      # program = Program.new(gl_context)
+      # program.attach_shader(shader)
+      shader.delete
+      expect(shader.deleted?).to be_true
+    end
+  end
+
+  describe "#type" do
+    Shader::Type.each do |type|
+      it "returns the shader type #{type}" do
+        shader = Shader.new(gl_context, type)
+        expect(shader.type).to eq(type)
+      end
+    end
+  end
+
+  describe "#source=" do
+    it "sets the shader source" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = VALID_SHADER_SOURCE
+      expect(shader.source).to eq(VALID_SHADER_SOURCE)
+    end
+
+    it "accepts multiple strings" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = VALID_SHADER_SOURCE.lines(false)
+      expect(shader.source).to eq(VALID_SHADER_SOURCE)
+    end
+  end
+
+  describe "#compile" do
+    it "compiles the shader" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = VALID_SHADER_SOURCE
+      expect(shader.compile).to be_true
+    end
+  end
+
+  describe "#compile!" do
+    it "compiles the shader" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = VALID_SHADER_SOURCE
+      expect { shader.compile! }.not_to raise_error
+    end
+
+    it "raises an error if the shader fails to compile" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = INVALID_SHADER_SOURCE
+      expect { shader.compile! }.to raise_error(Glint::ShaderCompilationError)
+    end
+  end
+
+  describe "#compiled?" do
+    it "returns true if the shader is compiled" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = VALID_SHADER_SOURCE
+      shader.compile
+      expect(shader.compiled?).to be_true
+    end
+
+    it "returns false if the shader is not compiled" do
+      shader = Shader.new(gl_context, :vertex)
+      expect(shader.compiled?).to be_false
+    end
+  end
+
+  describe "#info_log" do
+    it "returns the shader info log" do
+      shader = Shader.new(gl_context, :vertex)
+      shader.source = INVALID_SHADER_SOURCE
+      shader.compile
+      expect(shader.info_log).not_to be_empty
+    end
+  end
+
+  describe "#to_unsafe" do
+    it "returns the shader name" do
+      shader = Shader.new(gl_context, :vertex)
+      expect(shader.to_unsafe).to eq(shader.name)
     end
   end
 end
--- src/glint/shader.cr
+++ src/glint/shader.cr
@@ -1,6 +1,7 @@
 require "opengl"
 require "./context"
 require "./contextual"
+require "./errors"
 
 module Glint
   struct Shader
@@ -16,6 +17,7 @@ module Glint
     end
 
     getter context : Context
+    getter name : LibGL::UInt
 
     def initialize(@context : Context, @name : LibGL::UInt)
     end
@@ -27,6 +29,82 @@ module Glint
 
     def exists?
       gl.without_error_checking &.is_shader(@name) == LibGL::Boolean::True
+    end
+
+    def delete
+      gl.delete_shader(@name)
+    end
+
+    def deleted?
+      value = uninitialized LibGL::Int
+      gl.get_shader_iv(@name, LibGL::ShaderParameterName::DeleteStatus, pointerof(value))
+      value == LibGL::Boolean::True
+    end
+
+    def type : Type
+      value = uninitialized LibGL::Int
+      gl.get_shader_iv(@name, LibGL::ShaderParameterName::ShaderType, pointerof(value))
+      Type.new(value.to_u32!)
+    end
+
+    def source : String
+      length = source_length
+      String.new(length) do |buffer|
+        gl.get_shader_source(@name, length, pointerof(length), buffer)
+        {length, 0}
+      end
+    end
+
+    private def source_length
+      value = uninitialized LibGL::Int
+      gl.get_shader_iv(@name, LibGL::ShaderParameterName::ShaderSourceLength, pointerof(value))
+      value
+    end
+
+    def source=(source : String)
+      pointer = source.to_unsafe
+      null = Pointer(Int32).null
+      gl.shader_source(@name, 1, pointerof(pointer), null)
+    end
+
+    def source=(sources : Enumerable(String))
+      pointers = sources.map &.to_unsafe
+      null = Pointer(Int32).null
+      gl.shader_source(@name, pointers.size, pointers.to_unsafe, null)
+    end
+
+    def compile : Bool
+      gl.compile_shader(@name)
+      compiled?
+    end
+
+    def compile!
+      return if compile
+      raise ShaderCompilationError.new(info_log)
+    end
+
+    def compiled?
+      value = uninitialized LibGL::Int
+      gl.get_shader_iv(@name, LibGL::ShaderParameterName::CompileStatus, pointerof(value))
+      value != 0
+    end
+
+    def info_log
+      length = info_log_length
+      String.new(length) do |buffer|
+        gl.get_shader_info_log(@name, length, pointerof(length), buffer)
+        {length, 0}
+      end
+    end
+
+    private def info_log_length
+      value = uninitialized LibGL::Int
+      gl.get_shader_iv(@name, LibGL::ShaderParameterName::InfoLogLength, pointerof(value))
+      value
+    end
+
+    def to_unsafe
+      @name
     end
   end
 end
