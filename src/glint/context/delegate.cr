e0537add3604ca8d0f2ee20727b880a27f8b5b0d
Michael Miller <icy.arctic.fox@gmail.com>
2025-01-24 17:07:44 -0700

    Conditionally invoke block if an OpenGL function is available

--- src/glint/context/delegate.cr
+++ src/glint/context/delegate.cr
@@ -16,8 +16,13 @@ module Glint
       end
 
       macro method_missing(call)
-        proc = @loader.{{call.name}}!
-        proc.call({{call.args.splat}})
+        {% if call.block || call.block_arg %}
+          proc = @loader.{{call.name}}
+          yield proc if proc
+        {% else %}
+          proc = @loader.{{call.name}}!
+          proc.call({{call.args.splat}})
+        {% end %}
       end
     end
 
@@ -26,11 +31,28 @@ module Glint
         Delegate.new(@loader)
       end
 
+      private def wrap_gl_function(proc : P) : P forall P
+        {% begin %}
+          # Workaround for: https://github.com/crystal-lang/crystal/issues/15373
+          {% args = P.type_vars[0].type_vars.map_with_index { |_, i| "arg#{i}".id }.splat %}
+          P.new do |{{args}}|
+            with_error_checking do
+              proc.call({{args}})
+            end
+          end
+        {% end %}
+      end
+
       macro method_missing(call)
-        proc = @loader.{{call.name}}!
-        with_error_checking do
-          proc.call({{call.args.splat}})
-        end
+        {% if call.block || call.block_arg %}
+          proc = @loader.{{call.name}}
+          yield wrap_gl_function(proc) if proc
+        {% else %}
+          proc = @loader.{{call.name}}!
+          with_error_checking do
+            proc.call({{call.args.splat}})
+          end
+        {% end %}
       end
     end
   end
